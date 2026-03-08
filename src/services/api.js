ae44328925c9044efdff706694622e7721f99482
Dhanush <thannu3183@gmail.com>
2026-03-08 10:02:56 +0530

    Updated frontend project

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:9095/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
